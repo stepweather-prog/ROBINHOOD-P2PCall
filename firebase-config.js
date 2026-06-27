108fdd44cde1e1568af6fd8b914c96964f0eeb12
RobinHood P2P <stepweather@gmail.com>
2026-06-28 00:32:14 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js (НЕ КОММИТИТЬ В GIT!)
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDP-8Jpt3t-VQL9fOIlMGD4kpZtUdoW41Q",
+    authDomain: "robinhood-p2p-a59e1.firebaseapp.com",
+    databaseURL: "https://robinhood-p2p-a59e1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "robinhood-p2p-a59e1",
+    storageBucket: "robinhood-p2p-a59e1.firebasestorage.app",
+    messagingSenderId: "407723800099",
+    appId: "1:407723800099:web:5f20145d98bd35a28da396"
+};
